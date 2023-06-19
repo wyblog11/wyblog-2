01853cf21e100936ad6f9390480dcb5c12d93478
wuying <106313976+612901@users.noreply.github.com>
2023-06-19 20:52:27 +0800

    Update F12和复制.js

--- themes/butterfly/source/js/add/F12和复制.js
+++ themes/butterfly/source/js/add/F12和复制.js
@@ -1,3 +1,16 @@
+new Vue({
+    data: function () {
+        this.$notify({
+            title: "你已被发现😜",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+        });
+    }
+})
 // 复制提醒
 document.addEventListener("copy", function () {
   debounce(function () {
