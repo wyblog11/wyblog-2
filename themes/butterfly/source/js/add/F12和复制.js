45e990508414b43a648e25b76c0d82637c142429
wuying <106313976+612901@users.noreply.github.com>
2023-06-23 11:13:49 +0800

    Update F12和复制.js

--- themes/butterfly/source/js/add/F12和复制.js
+++ themes/butterfly/source/js/add/F12和复制.js
@@ -15,24 +15,3 @@ document.addEventListener("copy",function(e){
   })
 })
 
-/* 禁用F12按键并提醒 */
-document.onkeydown = function () {
-  if (window.event && window.event.keyCode == 123) {
-    event.keyCode = 0;
-    event.returnValue = false;
-    new Vue({
-      data:function(){
-        this.$notify({
-          title:"啊啊！你干嘛啊！",
-          message:"怎么能随随便便想扒猹的底裤呢？坏！",
-          position: 'bottom-right',
-          offset: 50,
-          showClose: false,
-          type:"error"
-        });
-        return{visible:false}
-      }
-    })
-    return false;
-  }
-};
