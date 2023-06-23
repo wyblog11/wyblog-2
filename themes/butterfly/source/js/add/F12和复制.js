a8d9a08cd83a902ea256427a30734006001d8863
wuying <106313976+612901@users.noreply.github.com>
2023-06-23 14:04:53 +0800

    Update F12和复制.js

--- themes/butterfly/source/js/add/F12和复制.js
+++ themes/butterfly/source/js/add/F12和复制.js
@@ -4,8 +4,8 @@ document.addEventListener("copy",function(e){
     data:function(){
       this.$notify({
         title:"哎嘿！复制成功",
-        message:"若要转载请务必保留原文链接！猹分你个瓜！",
-        position: 'bottom-right',
+        message:"若要转载请务必保留原文链接",
+        position: 'top-left',
         offset: 50,
         showClose: false,
         type:"success"
@@ -14,4 +14,23 @@ document.addEventListener("copy",function(e){
     }
   })
 })
+// f12提醒但不禁用
+document.onkeydown = function (e) {
+  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
+    debounce(function () {
+      new Vue({
+        data: function () {
+          this.$notify({
+            title: "你已被发现",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+          });
+          return{visible:false}
+        }
+      })
+    })
 
